bbbbf0c88faa801d210e1b3de4335356edf4102d
koreaioi <koreaioi@gachon.ac.kr>
2025-11-03 03:26:10 +0900

    feat(Application): Application 실행 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.config.ApplicationConfig;
+import lotto.controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ApplicationConfig applicationConfig = ApplicationConfig.getInstance();
+        LottoController lottoController = applicationConfig.lottoController();
+        lottoController.start();
     }
 }
