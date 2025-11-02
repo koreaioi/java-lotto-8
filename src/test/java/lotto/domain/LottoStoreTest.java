ac836e5276d700d2f51a440d23ec68a87253820f
koreaioi <koreaioi@gachon.ac.kr>
2025-11-02 21:30:13 +0900

    test(LottoStoreTest): 로또구매 테스트코드 추가

---
+++ src/test/java/lotto/domain/LottoStoreTest.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import lotto.domain.customer.*;
+import lotto.domain.customer.Number;
+import lotto.domain.store.LottoStore;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class LottoStoreTest {
+
+    @Test
+    void 로또_구매_테스트() {
+        Lotto testLotto = Lotto.from(List.of(
+                Number.from(1),
+                Number.from(2),
+                Number.from(3),
+                Number.from(4),
+                Number.from(5),
+                Number.from(6)
+        ));
+
+        Customer customer = Customer.of(Money.from(1000));
+        LottoTestGenerator lottoTestGenerator = new LottoTestGenerator(List.of(testLotto));
+        LottoStore lottoStore = new LottoStore(lottoTestGenerator);
+        customer.buyLotto(lottoStore);
+
+        Assertions.assertThat(customer.toDisplay())
+                .isEqualTo(testLotto.toDisplay());
+    }
+
+}
