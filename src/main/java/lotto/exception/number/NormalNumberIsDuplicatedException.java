2bf2ef4ecce44fe9c31989e1d4e4ec34cee98526
koreaioi <koreaioi@gachon.ac.kr>
2025-11-03 00:05:06 +0900

    feat(NormalNumberIsDuplicatedException): 일반 로또 번호끼리 중복되는 예외 추가

---
+++ src/main/java/lotto/exception/number/NormalNumberIsDuplicatedException.java
@@ -0,0 +1,10 @@
+package lotto.exception.number;
+
+import lotto.exception.ErrorMessage;
+import lotto.exception.LottoException;
+
+public class NormalNumberIsDuplicatedException extends LottoException {
+    public NormalNumberIsDuplicatedException() {
+        super(ErrorMessage.NORMAL_NUMBER_IS_DUPLICATED.getMessage());
+    }
+}
