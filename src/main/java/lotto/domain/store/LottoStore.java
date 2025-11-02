ea8552e9d1d957ec29c0225cd21593fc7399f22e
koreaioi <koreaioi@gachon.ac.kr>
2025-11-02 20:54:36 +0900

    feat(LottoStore): 로또 판매 메서드 추가

--- src/main/java/lotto/domain/store/LottoStore.java
+++ src/main/java/lotto/domain/store/LottoStore.java
@@ -1,11 +1,24 @@
 package lotto.domain.store;
 
+import lotto.domain.customer.Lotto;
+import lotto.domain.customer.LottoBundle;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class LottoStore {
 
     private final LottoGenerator lottoGenerator;
 
     public LottoStore(LottoGenerator lottoGenerator) {
         this.lottoGenerator = lottoGenerator;
+    }
+
+    public LottoBundle sellLotto(int quantity) {
+        List<Lotto> lottoBundle = IntStream.range(0, quantity)
+                .mapToObj(i -> lottoGenerator.generateLotto())
+                .toList();
+        return LottoBundle.from(lottoBundle);
     }
 
 }
