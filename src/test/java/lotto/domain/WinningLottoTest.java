484993ae7c6fd7bad9f47e0e9f35b4608aac531a
koreaioi <koreaioi@gachon.ac.kr>
2025-11-03 00:55:27 +0900

    test(WinningLottoTest): 로또 당첨 순위를 테스트하는 코드 추가

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import lotto.domain.customer.Lotto;
 import lotto.domain.customer.Number;
+import lotto.domain.processor.LottoRank;
 import lotto.domain.processor.WinningLotto;
 import lotto.exception.number.BonusNumberIsDuplicatedException;
 import lotto.exception.number.NormalNumberIsDuplicatedException;
@@ -11,6 +12,11 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class WinningLottoTest {
+
+    private static final int BONUS = 7;
+    private static final int NOT_MATCH_8 = 8;
+    private static final int NOT_MATCH_9 = 9;
+    private static final int NOT_MATCH_10 = 10;
 
     @Test
     void 당첨번호에_중복이존재하면_예외가_발생한다() {
@@ -30,6 +36,30 @@ public class WinningLottoTest {
         Assertions.assertThatThrownBy(() -> WinningLotto.of(lotto, duplicatedBonusNumber))
                 .isInstanceOf(BonusNumberIsDuplicatedException.class)
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또당첨순위를_테스트한다() {
+        Lotto testLotto = Lotto.from(getTestLottoNumbers(List.of(1, 2, 3, 4, 5, 6)));
+        Number bonusNumber = Number.from(BONUS);
+        WinningLotto winningLotto = WinningLotto.of(testLotto, bonusNumber);
+
+        Lotto firstLotto = Lotto.from(getTestLottoNumbers(List.of(1, 2, 3, 4, 5, 6)));
+        Lotto secondLotto = Lotto.from(getTestLottoNumbers(List.of(1, 2, 3, 4, 5, BONUS)));
+        Lotto thirdLotto = Lotto.from(getTestLottoNumbers(List.of(1, 2, 3, 4, 5, NOT_MATCH_8)));
+        Lotto fourthLotto = Lotto.from(getTestLottoNumbers(List.of(1, 2, 3, 4, NOT_MATCH_9, NOT_MATCH_8)));
+        Lotto fifthLotto = Lotto.from(getTestLottoNumbers(List.of(1, 2, 3, NOT_MATCH_10, NOT_MATCH_9, NOT_MATCH_8)));
+
+        Assertions.assertThat(winningLotto.determineRank(firstLotto))
+                .isEqualTo(LottoRank.FIRST);
+        Assertions.assertThat(winningLotto.determineRank(secondLotto))
+                .isEqualTo(LottoRank.SECOND);
+        Assertions.assertThat(winningLotto.determineRank(thirdLotto))
+                .isEqualTo(LottoRank.THIRD);
+        Assertions.assertThat(winningLotto.determineRank(fourthLotto))
+                .isEqualTo(LottoRank.FOURTH);
+        Assertions.assertThat(winningLotto.determineRank(fifthLotto))
+                .isEqualTo(LottoRank.FIFTH);
     }
 
     private List<Number> getTestLottoNumbers(List<Integer> numbers) {
