1685fb94c15fb530c4c5a8a0ba6eb4c3bcf24cb1
koreaioi <koreaioi@gachon.ac.kr>
2025-11-02 18:31:59 +0900

    feat(Number): 로또 번호가 음수인 경우를 검증하는 메서드 추가

--- src/main/java/lotto/domain/customer/Number.java
+++ src/main/java/lotto/domain/customer/Number.java
@@ -1,5 +1,6 @@
 package lotto.domain.customer;
 
+import lotto.exception.number.NumberIsNegativeException;
 import lotto.exception.number.NumberIsZeroException;
 
 public class Number {
@@ -18,11 +19,18 @@ public class Number {
     private void validate(int number) {
         // TODO(should) 검증 추가하기
         validateZero(number);
+        validateNegative(number);
     }
 
     private void validateZero(int number) {
         if (number == 0) {
             throw new NumberIsZeroException();
+        }
+    }
+
+    private void validateNegative(int number) {
+        if (number < 0) {
+            throw new NumberIsNegativeException();
         }
     }
 
