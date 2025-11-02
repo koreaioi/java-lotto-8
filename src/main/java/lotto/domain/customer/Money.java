9586ff44f3257316a927eb10ca67da36cfeaf92b
koreaioi <koreaioi@gachon.ac.kr>
2025-11-02 17:30:33 +0900

    feat(Money): 구입 금액이 0인 경우 검증 메서드 추가

--- src/main/java/lotto/domain/customer/Money.java
+++ src/main/java/lotto/domain/customer/Money.java
@@ -1,15 +1,29 @@
 package lotto.domain.customer;
 
+import lotto.exception.MoneyIsZeroException;
+
 public class Money {
 
     private final int symbol;
 
     private Money(int money) {
+        validate(money);
         this.symbol = money;
     }
 
     public static Money from(int money) {
         return new Money(money);
     }
+
+    private void validate(int money) {
+        validateZero(money);
+    }
+
+    private void validateZero(int money) {
+        if (money == 0) {
+            throw new MoneyIsZeroException();
+        }
+    }
+
 
 }
