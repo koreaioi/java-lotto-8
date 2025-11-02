db552858aaa17aa7285f4b108da52d6b94957f3a
koreaioi <koreaioi@gachon.ac.kr>
2025-11-03 01:09:42 +0900

    feat(InputReader): 문자열을 입력받는 메서드 추가

--- src/main/java/lotto/view/InputReader.java
+++ src/main/java/lotto/view/InputReader.java
@@ -2,11 +2,20 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class InputReader {
 
     public String readValue() {
         return Console.readLine()
                 .trim();
+    }
+
+    public List<String> readValues() {
+        return Arrays.stream(Console.readLine().split(","))
+                .map(String::trim)
+                .toList();
     }
 
 }
