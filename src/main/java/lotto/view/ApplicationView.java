ba9e84968a9313df2995354bd698d296564bb45a
koreaioi <koreaioi@gachon.ac.kr>
2025-11-02 18:08:01 +0900

    feat(ApplicationView): ApplicationView 생성

---
+++ src/main/java/lotto/view/ApplicationView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+public class ApplicationView {
+
+    private final InputReader reader;
+    private final OutputWriter writer;
+
+
+    public ApplicationView(InputReader reader, OutputWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+}
