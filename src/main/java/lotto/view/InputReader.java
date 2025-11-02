34fb5a25362a64b7e9bb58f16c4de8f439dda376
koreaioi <koreaioi@gachon.ac.kr>
2025-11-02 16:58:33 +0900

    feat(InputReader): InputReader 생성, 문자열을 읽어들이는 기능 추가

---
+++ src/main/java/lotto/view/InputReader.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputReader {
+
+    public String readValue() {
+        return Console.readLine()
+                .trim();
+    }
+
+}
