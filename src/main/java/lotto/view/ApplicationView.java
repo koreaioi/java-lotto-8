a889ee991fc47d94b2e6577fbd2404bcce3b126c
koreaioi <koreaioi@gachon.ac.kr>
2025-11-02 18:08:53 +0900

    feat(ApplicationView): Money를 요청받는 메서드 추가

--- src/main/java/lotto/view/ApplicationView.java
+++ src/main/java/lotto/view/ApplicationView.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import lotto.domain.customer.Money;
+import lotto.exception.MoneyIsNotIntegerException;
+
 public class ApplicationView {
 
     private final InputReader reader;
@@ -9,6 +12,27 @@ public class ApplicationView {
     public ApplicationView(InputReader reader, OutputWriter writer) {
         this.reader = reader;
         this.writer = writer;
+    }
+
+    public Money getMoney() {
+        writer.printMoneyRequestMessage();
+        while (true) {
+            try {
+                String value = reader.readValue();
+                validateParsingInteger(value);
+                return Money.from(Integer.parseInt(value));
+            } catch (IllegalArgumentException e) {
+                writer.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private void validateParsingInteger(String value) {
+        try{
+            Integer.parseInt(value);
+        }catch (NumberFormatException e){
+            throw new MoneyIsNotIntegerException();
+        }
     }
 
 }
