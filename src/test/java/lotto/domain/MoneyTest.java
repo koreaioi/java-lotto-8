f08e5fc4f9e7bda1e2ca5b73e134aaf10944631c
koreaioi <koreaioi@gachon.ac.kr>
2025-11-02 17:48:24 +0900

    test(MoneyTest): 테스트 변수의 의미가 잘 드러나도록 테스트 변수명 변경

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -11,28 +11,36 @@ public class MoneyTest {
 
     @Test
     void 구입금액이_0이면_예외가_발생한다() {
-        Assertions.assertThatThrownBy(() -> Money.from(0))
+        int zero = 0;
+
+        Assertions.assertThatThrownBy(() -> Money.from(zero))
                 .isInstanceOf(MoneyIsZeroException.class)
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 구입금액이_음수이면_예외가_발생한다() {
-        Assertions.assertThatThrownBy(() -> Money.from(-1000))
+        int negative = -1000;
+
+        Assertions.assertThatThrownBy(() -> Money.from(negative))
                 .isInstanceOf(MoneyIsNegativeException.class)
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 구입금액이_1000단위가__아니면_예외가_발생한다() {
-        Assertions.assertThatThrownBy(() -> Money.from(500))
+        int notThousandUnit = 500;
+
+        Assertions.assertThatThrownBy(() -> Money.from(notThousandUnit))
                 .isInstanceOf(MoneyIsNotThousandUnitException.class)
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 구입금액이_정상적으로_인식되는지_테스트한다() {
-        Assertions.assertThatCode(() -> Money.from(5000))
+        int normalMoney = 5000;
+
+        Assertions.assertThatCode(() -> Money.from(normalMoney))
                 .doesNotThrowAnyException();
     }
 
