1e5bb592ce5258c8d5066cc31d25b63669f00752
koreaioi <koreaioi@gachon.ac.kr>
2025-11-02 17:46:33 +0900

    test(MoneyTest): 구입 금액이 1000원 단위의 양수인 경우 잘 작동하는 지 테스트

--- src/test/java/lotto/domain/MoneyTest.java
+++ src/test/java/lotto/domain/MoneyTest.java
@@ -5,30 +5,35 @@ import lotto.exception.MoneyIsNegativeException;
 import lotto.exception.MoneyIsNotThousandUnitException;
 import lotto.exception.MoneyIsZeroException;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.assertj.core.api.Assertions;
 
 public class MoneyTest {
 
     @Test
     void 구입금액이_0이면_예외가_발생한다() {
-        assertThatThrownBy(() -> Money.from(0))
+        Assertions.assertThatThrownBy(() -> Money.from(0))
                 .isInstanceOf(MoneyIsZeroException.class)
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 구입금액이_음수이면_예외가_발생한다() {
-        assertThatThrownBy(() -> Money.from(-1000))
+        Assertions.assertThatThrownBy(() -> Money.from(-1000))
                 .isInstanceOf(MoneyIsNegativeException.class)
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 구입금액이_1000단위가__아니면_예외가_발생한다() {
-        assertThatThrownBy(() -> Money.from(500))
+        Assertions.assertThatThrownBy(() -> Money.from(500))
                 .isInstanceOf(MoneyIsNotThousandUnitException.class)
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 구입금액이_정상적으로_인식되는지_테스트한다() {
+        Assertions.assertThatCode(() -> Money.from(5000))
+                .doesNotThrowAnyException();
     }
 
 }
