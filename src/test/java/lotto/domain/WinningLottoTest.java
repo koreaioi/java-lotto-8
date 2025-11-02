dc402eb570f25f4da57a110283918b74d7f5b83d
koreaioi <koreaioi@gachon.ac.kr>
2025-11-03 00:40:47 +0900

    test(WinningLottoTest): 보너스번호가 당첨번호와 중복되면 예외가 발생하는 테스트 코드 추가

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import lotto.domain.customer.Lotto;
 import lotto.domain.customer.Number;
+import lotto.domain.processor.WinningLotto;
+import lotto.exception.number.BonusNumberIsDuplicatedException;
 import lotto.exception.number.NormalNumberIsDuplicatedException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -23,6 +25,24 @@ public class WinningLottoTest {
 
         Assertions.assertThatThrownBy(() -> Lotto.from(duplicatedNumbers))
                 .isInstanceOf(NormalNumberIsDuplicatedException.class)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 보너스번호가_당첨번호와_중복되면_예외가_발생한다() {
+        List<Number> numbers = List.of(
+                Number.from(1),
+                Number.from(2),
+                Number.from(3),
+                Number.from(4),
+                Number.from(5),
+                Number.from(6)
+        );
+        Lotto lotto = Lotto.from(numbers);
+        Number duplicatedBonusNumber = Number.from(6);
+
+        Assertions.assertThatThrownBy(() -> WinningLotto.of(lotto, duplicatedBonusNumber))
+                .isInstanceOf(BonusNumberIsDuplicatedException.class)
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
