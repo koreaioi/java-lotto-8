5d366a02ee77cc8eede12772961be792a4360565
koreaioi <koreaioi@gachon.ac.kr>
2025-11-02 18:30:01 +0900

    feat(Number): 로또 번호가 0인 경우를 검증하는 메서드 추가

--- src/main/java/lotto/domain/customer/Number.java
+++ src/main/java/lotto/domain/customer/Number.java
@@ -1,16 +1,29 @@
 package lotto.domain.customer;
 
+import lotto.exception.number.NumberIsZeroException;
+
 public class Number {
 
     private final int symbol;
 
-    private Number(int symbol) {
-        // TODO(should) 검증 추가
-        this.symbol = symbol;
+    private Number(int number) {
+        validate(number);
+        this.symbol = number;
     }
 
-    public static Number from(int symbol) {
-        return new Number(symbol);
+    public static Number from(int number) {
+        return new Number(number);
+    }
+
+    private void validate(int number) {
+        // TODO(should) 검증 추가하기
+        validateZero(number);
+    }
+
+    private void validateZero(int number) {
+        if (number == 0) {
+            throw new NumberIsZeroException();
+        }
     }
 
 }
