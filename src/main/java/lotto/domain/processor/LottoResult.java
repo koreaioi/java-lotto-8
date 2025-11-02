9f519ed496f0fd96be070522ccb0d46564cfecf1
koreaioi <koreaioi@gachon.ac.kr>
2025-11-03 01:03:22 +0900

    refactor(LottoResult): 초기값이 없을 경우를 대비하여 getOrDefault로 수정

--- src/main/java/lotto/domain/processor/LottoResult.java
+++ src/main/java/lotto/domain/processor/LottoResult.java
@@ -18,7 +18,7 @@ public class LottoResult {
     private EnumMap<LottoRank, Integer> doStatistics(List<LottoRank> rankResult) {
         EnumMap<LottoRank, Integer> statistics = new EnumMap<>(LottoRank.class);
         for (LottoRank rank : rankResult) {
-            statistics.put(rank, statistics.get(rank) + 1);
+            statistics.put(rank, statistics.getOrDefault(rank, 0) + 1);
         }
         return statistics;
     }
