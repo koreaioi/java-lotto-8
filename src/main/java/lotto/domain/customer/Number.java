3760f38eee832d978ee6c57fe728bae5ca99c0ad
koreaioi <koreaioi@gachon.ac.kr>
2025-11-02 18:39:25 +0900

    refactor(Number): 검증과정을 하나로 축약 가능

--- src/main/java/lotto/domain/customer/Number.java
+++ src/main/java/lotto/domain/customer/Number.java
@@ -1,39 +1,18 @@
 package lotto.domain.customer;
 
-import lotto.exception.number.NumberIsNegativeException;
 import lotto.exception.number.NumberIsNotLottoRangeException;
-import lotto.exception.number.NumberIsZeroException;
 
 public class Number {
 
     private final int symbol;
 
     private Number(int number) {
-        validate(number);
+        validateLottoRange(number);
         this.symbol = number;
     }
 
     public static Number from(int number) {
         return new Number(number);
-    }
-
-    private void validate(int number) {
-        // TODO(should) 검증 추가하기
-        validateZero(number);
-        validateNegative(number);
-        validateLottoRange(number);
-    }
-
-    private void validateZero(int number) {
-        if (number == 0) {
-            throw new NumberIsZeroException();
-        }
-    }
-
-    private void validateNegative(int number) {
-        if (number < 0) {
-            throw new NumberIsNegativeException();
-        }
     }
 
     private void validateLottoRange(int number) {
--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -7,8 +7,6 @@ public enum ErrorMessage {
     MONEY_IS_NOT_THOUSAND_UNIT("1,000원 단위의 구입 금액을 다시 입력해주세요."),
     MONEY_IS_NOT_INTEGER("구입 금액이 숫자가 아닙니다. 1,000원 단위의 양수를 다시 입력해주세요."),
 
-    LOTTO_IS_ZERO("로또 번호는 0일 수 없습니다. 1과 45사이의 로또 번호를 다시 입력해주세요."),
-    LOTTO_IS_NEGATIVE("로또 번호는 음수일 수 없습니다. 1과 45사이의 로또 번호를 다시 입력해주세요."),
     NUMBER_IS_NOT_LOTTO_RANGE("로또 번호는 1부터 45 사이의 정수입니다. 다시 입력해주세요."),
     ;
 
