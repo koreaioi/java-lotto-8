6a64a22cea503ed68b107b0e77a73ed1218740fb
koreaioi <koreaioi@gachon.ac.kr>
2025-11-02 20:54:19 +0900

    feat(LottoStore): 로또 판매점 생성

---
+++ src/main/java/lotto/domain/store/LottoStore.java
@@ -0,0 +1,11 @@
+package lotto.domain.store;
+
+public class LottoStore {
+
+    private final LottoGenerator lottoGenerator;
+
+    public LottoStore(LottoGenerator lottoGenerator) {
+        this.lottoGenerator = lottoGenerator;
+    }
+
+}
