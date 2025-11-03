8bb0e0b863727363d3b32f54ef358bb14be03df3
koreaioi <koreaioi@gachon.ac.kr>
2025-11-03 13:56:08 +0900

    refactor(ApplicationView): 자바 컨벤션 - 개행 2칸을 1칸으로 수정

--- src/main/java/lotto/view/ApplicationView.java
+++ src/main/java/lotto/view/ApplicationView.java
@@ -20,7 +20,6 @@ public class ApplicationView {
     private final InputReader reader;
     private final OutputWriter writer;
 
-
     public ApplicationView(InputReader reader, OutputWriter writer) {
         this.reader = reader;
         this.writer = writer;
