662f21bab1385405628e545a359879b887f86509
koreaioi <koreaioi@gachon.ac.kr>
2025-11-03 22:55:50 +0900

    refactor(LottoRank): 세미콜론 오타 제거

--- src/main/java/lotto/domain/customer/LottoRank.java
+++ src/main/java/lotto/domain/customer/LottoRank.java
@@ -14,7 +14,7 @@ public enum LottoRank {
 
     private static final String EMPTY_STING = "";
     private static final String BONUS_MATCHING_MESSAGE = ", 보너스 볼 일치";
-    private static final String LOTTO_RANK_DISPLAY_FORMAT = "%d개 일치%s (%,d원);";
+    private static final String LOTTO_RANK_DISPLAY_FORMAT = "%d개 일치%s (%,d원)";
 
     private final int normalCount;
     private final long prizeMoney;
