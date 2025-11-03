7ded98efdfd18373e165daa47b08b491f4fa35d0
koreaioi <koreaioi@gachon.ac.kr>
2025-11-03 13:37:35 +0900

    refactor(LottoRandomGenerator): 로또 번호 범위를 상수처리

--- src/main/java/lotto/domain/store/LottoRandomGenerator.java
+++ src/main/java/lotto/domain/store/LottoRandomGenerator.java
@@ -8,9 +8,13 @@ import java.util.List;
 
 public class LottoRandomGenerator implements LottoGenerator {
 
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_COUNT = 6;
+
     @Override
     public Lotto generateLotto() {
-        List<Number> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        List<Number> numbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_COUNT)
                 .stream()
                 .map(Number::from)
                 .toList();
